4d3bfb373cd07558fcea2e0bd562f5710332a586
ldw <940799196@qq.com>
2017-09-01 11:06:13 +0800

    add view

--- Mysql/MySQL.sql
+++ Mysql/MySQL.sql
@@ -12,7 +12,7 @@ ALTER TABLE OLDTABLENAME RENAME NEWTABLENAME; //修改表名
 
 ALTER TABLE TABLENAME MODIFY 属性名 属性类型; //修改表数据类型
 
-ALTER TABEL TABLENAME CHAINED 旧属性名 新属性名  属性类型;//修改表属性名
+ALTER TABEL TABLENAME CHANGE 旧属性名 新属性名  属性类型;//修改表属性名
 
 ALTER TABEL TABLENAME ADD 属性名 属性类型[完整性约束条件] ;//添加表属性
 
@@ -40,7 +40,7 @@ SELECT DISTINCT 列表属性 FROM TABLENAME ; //查询结果不重复
 
 SELECT * FROM GROUP BY 列表属性 ASC/DESC，列表属性 ASC/DESC;  //排序 ASC升序 DESC降序 
 
-SELECT 需要分组的列表属性 GROUP_CONCAT(列表属性) FROM  表名 GROUP BY 列表属性; //分组查询 
+SELECT 需要分组的列表属性 GROUP_CONCAT(列表属性) FROM  表名 GROUP BY 分组属性; //分组查询 
 
 SELECT * FROM 表名 LIMIT 初始位置，记录条数  //分页查询 
 
@@ -60,17 +60,51 @@ CREATE SPATIAL INDEX 索引名 ON 表名 （属性名 ASC/DESC）//创建空间�
 
 CREATE FULLTEXT INDEX 索引名 ON 表名 （属性名 ASC/DESC）//创建全文索引
 
-DROP INDEX 索引名 ON 表名
+DROP INDEX 索引名 ON 表名  //移除索引
 
+CREATE/REPLACE VIEW VIEWNAME(属性列表) AS SELECT(属性列表) FROM TABELNAME WITH LOCAL CHECK OPTION ;//创建视图 /修改视图
 
+ALTER VIEW VIEWNAME(列表属性) SELECT (列表属性) FROM TABELNAME WHILE LOCAL CHECK OPTION;//修改视图 
 
+UPDATE VIEW VIENAME SET 属性="属性值" WHERE 条件; //更新视图数据
 
-
-
-
-
-
+如果视图包含下述结构中的任何一种，那么它就是不可更新的：
+（1）聚合函数；
  
+（2）DISTINCT关键字；
+ 
+（3）GROUP BY子句；
+ 
+（4）ORDER BY子句；
+ 
+（5）HAVING子句；
+ 
+（6）UNION运算符；
+ 
+（7）位于选择列表中的子查询；
+ 
+（8）FROM子句中包含多个表；
+ 
+（9）SELECT语句中引用了不可更新视图；
+ 
+（10）WHERE子句中的子查询，引用FROM子句中的表；
+ 
+（11）ALGORITHM 选项指定为TEMPTABLE（使用临时表总会使视图成为不可更新的）。
+
+INSERT INTO VIENAME VALUE();//视图插入数据
+
+DELETE FROM VIENAME WHERE 条件; //删除数据
+
+DROP VIEW VIENAME; //移除视图
+
+
+
+
+
+
+
+
+
 
 
 
